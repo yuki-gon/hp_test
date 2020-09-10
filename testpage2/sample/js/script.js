0e680d1c681ef50b3cc68989094dbc3c7df2a694
yukigon2896 <yukigon2896@gmail.com>
2020-09-10 12:43:40 +0900

    ダークモード

---
+++ testpage2/sample/js/script.js
@@ -0,0 +1,19 @@
+function changestylesheet_right() {
+//    let test_text_tmp;
+//    test_text_tmp = document.getElementById('test_text');
+//    test_text_tmp.innerHTML = '<h1>れんしゅうううう</h1>';
+
+    document.getElementById('style-a').disabled = true;
+    document.getElementById('style-b').disabled = false;
+}
+
+function changestylesheet_dark() {
+    //    let test_text_tmp;
+    //    test_text_tmp = document.getElementById('test_text');
+    //    test_text_tmp.innerHTML = '<h1>れんしゅうううう</h1>';
+
+    document.getElementById('style-a').disabled = false;
+    document.getElementById('style-b').disabled = true;
+    
+    }
+    
