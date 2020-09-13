a3108f7749d224cdc9df66cf63a6ba9719ad2658
yukigon2896 <yukigon2896@gmail.com>
2020-09-13 10:27:28 +0900

    ダークモード適応

---
+++ testpage2/js/darkmode.js
@@ -0,0 +1,11 @@
+function changestylesheet_right() {
+    document.getElementById('style-a').disabled = true;
+    document.getElementById('style-b').disabled = false;
+}
+
+function changestylesheet_dark() {
+
+    document.getElementById('style-a').disabled = false;
+    document.getElementById('style-b').disabled = true;
+    
+    }
