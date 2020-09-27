25d143235db961b2a1988cc0a0a1d8950a6b4126
yukigon2896 <yukigon2896@gmail.com>
2020-09-27 11:30:48 +0900

    目次とページ間のダークモード

--- testpage2/js/darkmode.js
+++ testpage2/js/darkmode.js
@@ -25,11 +25,30 @@ function hensu_test2(page_no) {
     }
 }
 
-function hensu_test3(page_no) {
-    let hikisu = document.getElementById("btn-c");
-    if(document.getElementById('style-light').disabled){
-        hikisu.href ="ss/ep" + page_no + ".html?hikisu=dark";
-    }else{
+function hensu_test3(page_no , id_name) {
+    let hikisu = document.getElementById(id_name);
+    if(document.getElementById('style-dark').disabled){
         hikisu.href ="ss/ep" + page_no + ".html?hikisu=light";
+    }else{
+        hikisu.href ="ss/ep" + page_no + ".html?hikisu=dark";
+    }
+}
+
+function hensu_test4(page_no , id_name) {
+    let hikisu = document.getElementById(id_name);
+    if(document.getElementById('style-dark').disabled){
+        //trueであれば非表示なのでdarkが非表示だからlight
+        hikisu.href ="ep" + page_no + ".html?hikisu=light";
+    }else{
+        hikisu.href ="ep" + page_no + ".html?hikisu=dark";
+    }
+}
+
+function hensu_home(id_name){
+    let hikisu = document.getElementById(id_name);
+    if(document.getElementById('style-dark').disabled){
+        hikisu.href="../index.html?hikisu=light";
+    }else{
+        hikisu.href="../index.html?hikisu=dark";
     }
 }
--- testpage2/sample/js/script.js
+++ testpage2/sample/js/script.js
@@ -16,4 +16,14 @@ function changestylesheet_dark() {
     document.getElementById('style-light').disabled = true;
     
     }
+
+    function hensu_test3(page_no , id_name) {
+        let hikisu = document.getElementById(id_name);
+        if(document.getElementById('style-dark').disabled){
+            //trueであれば非表示なのでdarkが非表示だからlight
+            hikisu.href ="ss/ep" + page_no + ".html?hikisu=light";
+        }else{
+            hikisu.href ="ss/ep" + page_no + ".html?hikisu=dark";
+        }
+    }
     
